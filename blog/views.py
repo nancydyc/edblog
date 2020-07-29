13ef83a0923f68ccbcbf274e004f682207d72546
Yichen <msdaiyichen@gmail.com>
2020-07-28 23:25:14 -0700

    Add context in the view func to be used in homepage

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author' : 'Yichen',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Jul 28, 2020',
+    },
+    {
+        'author' : 'Nancy',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Jul 28, 2020',
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
